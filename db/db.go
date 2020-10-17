cad96234ba199230737ebd8cf495e1d2a4a805b7
Komaricus <komarovshura@yandex.ru>
2020-10-17 13:15:40 +0300

    Fixes timezone in connection

--- db/db.go
+++ db/db.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func Init(configuration config.Configuration) error {
-	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", configuration.DB_HOST, configuration.DB_USERNAME, configuration.DB_PASSWORD, configuration.DB_NAME, configuration.DB_PORT)
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=posix/Europe/Moscow", configuration.DB_HOST, configuration.DB_USERNAME, configuration.DB_PASSWORD, configuration.DB_NAME, configuration.DB_PORT)
 	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		return err
